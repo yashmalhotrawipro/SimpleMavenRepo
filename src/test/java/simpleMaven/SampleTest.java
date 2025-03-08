e0a10e9cdd58fe6c9c14f42170fa781b88fde487
b60user04 <b63user04@rpsconsulting.in>
2025-03-08 02:09:30 -0800

    initial commit

---
+++ src/test/java/simpleMaven/SampleTest.java
@@ -0,0 +1,14 @@
+package simpleMaven;
+
+
+
+import org.junit.Test;
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+		System.out.println("Simple git maven project");
+	}
+
+}
---
+++ src/test/java/simpleMaven/Tc2.java
@@ -0,0 +1,14 @@
+package simpleMaven;
+
+
+
+import org.junit.Test;
+
+public class Tc2 {
+
+	@Test
+	public void test() {
+		System.out.println("TestCase2");
+	}
+
+}
