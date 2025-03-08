2304be1b1226eb3a50fceba2f91163aec2cbf4c8
b60user04 <b63user04@rpsconsulting.in>
2025-03-08 02:22:24 -0800

    tc3

---
+++ src/test/java/simpleMaven/TC3.java
@@ -0,0 +1,19 @@
+package simpleMaven;
+
+
+
+import org.junit.After;
+import org.junit.Test;
+
+public class TC3 {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		System.out.println("Tc3 running");
+	}
+
+}
